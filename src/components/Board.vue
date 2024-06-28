7ea19585c0d00cc07893b2bc2f3e43fbc32bc6e6
Akul Chhillar <akul.chhillar@gartner.com>
2024-06-28 06:15:40 +0530

    removed local storage

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,24 +1,27 @@
 <template>
     <div class="flex flex-col">
     <Navbar/>
+    
 <div class="bg-stone-100 min-w-max min-h-screen flex flex-col items-center font-Poppins border-2
  border-stone-100">
 
- <div class="mt-24">
 
- <div class="card bg-blue-50 w-96 rounded-md flex flex-col justify-center items-center">
+ <div class="mt-24">
+    <div v-show="show_skeleton" class="skeleton h-32 w-96"></div> 
+ <div v-show="show_skeleton==false" class="card bg-blue-50 w-96 rounded-md flex flex-col justify-center items-center">
  <div class="p-4 min-w-full">
-    <div class="bg-blue-100 font-medium flex flex-row p-4 justify-center items-center font-Poppins rounded-t-md capitalize">{{ clues[0] }}</div>
-    <div class="bg-blue-200 font-medium flex flex-row p-4 justify-center items-center font-Poppins capitalize transition-all">{{ hidden_clues[0] }}</div>
-    <div class="bg-blue-300 font-medium flex flex-row p-4 justify-center items-center font-Poppins capitalize">{{ hidden_clues[1] }}</div>
-    <div class="bg-blue-400 font-medium flex flex-row p-4 justify-center items-center font-Poppins capitalize">{{ hidden_clues[2] }}</div>
-    <div class="bg-blue-500 font-medium flex flex-row p-4 justify-center items-center font-Poppins capitalize" :class="gameongoing==true ? 'rounded-b-md' :''  ">{{ hidden_clues[3] }}</div>
-    <div class="bg-blue-600 font-medium flex flex-row p-4 justify-center items-center font-Poppins rounded-b-md capitalize" v-if="gameongoing==false">{{status_msg}}</div>
+    <div class="bg-blue-100 font-medium flex flex-row p-4 justify-center items-center  rounded-t-md capitalize">{{ clues[0] }}</div>
+    <div class="bg-blue-200 font-medium flex flex-row p-4 justify-center items-center  capitalize transition-all">{{ hidden_clues[0] }}</div>
+    <div class="bg-blue-300 font-medium flex flex-row p-4 justify-center items-center  capitalize">{{ hidden_clues[1] }}</div>
+    <div class="bg-blue-400 font-medium flex flex-row p-4 justify-center items-center  capitalize">{{ hidden_clues[2] }}</div>
+    <div class="bg-blue-500 font-medium flex flex-row p-4 justify-center items-center  capitalize" :class="gameongoing==true ? 'rounded-b-md' :''  ">{{ hidden_clues[3] }}</div>
+    <div class="bg-blue-600 font-medium flex flex-row p-4 justify-center items-center  rounded-b-md capitalize" v-show="gameongoing==false">{{word}}</div>
     
-    <div class="bg-white mt-2 font-medium flex flex-row p-4 gap-2 justify-center items-center font-Poppins rounded-b-md" v-show="gameongoing==true">
+    <div class="bg-white mt-2 font-medium flex flex-row p-4 gap-2 justify-center items-center  rounded-b-md">
  
-        <span class="line-through" v-for="word in attempted_words" v-show="(guess_count>0)">{{word}}</span>
+        <span class="line-through" v-for="word in attempted_words" v-show="(guess_count>0) && (gameongoing==true)">{{word}}</span>
         <span v-show="(guess_count===0)">All 5 clues belong to a common word. Guess the common word in as few clues as possible.</span>
+        <span v-show="gameongoing==false">{{ status_msg }}</span>
     </div>
 </div>
 
@@ -27,12 +30,12 @@
 
 
 
-<div class="mt-2">
+<div class="mt-2" v-show="show_skeleton==false" >
 
     <label class="input input-lg flex flex-row justify-center 
 items-center rounded-b-md w-96 shadow-xl">
-  <input type="text" :disabled="gameongoing==false" class="grow font-Poppins" placeholder="Guess the word" v-model="current_guess" />
-  <button :disabled="gameongoing==false" class="badge badge-lg bg-base-300 font-Poppins" @click="guess(current_guess)">Guess</button>
+  <input type="text" :disabled="gameongoing==false" class="grow " placeholder="Guess the word" v-model="current_guess" />
+  <button :disabled="gameongoing==false" class="badge badge-lg bg-base-300 " @click="guess(current_guess)">Guess</button>
 </label>
 
 </div>
@@ -42,7 +45,7 @@ items-center rounded-b-md w-96 shadow-xl">
 
 </div>
 </div>
-
+{{ word }}
 </template>
 
 <script setup>
@@ -60,22 +63,23 @@ let word = ref(null)
 let gameongoing = ref(true)
 let msgs = ref([])
 let status_msg = ref(null)
+let show_skeleton = ref(true)
 
 msgs = ["🎉 Woohoo You won!","😔 Better luck next time."]
 
 onMounted(async()=>{
     
 
-if(localStorage.getItem('gameongoing')){
-    if (localStorage.getItem('gameongoing')=='false'){
-        gameongoing.value = false
-    }
-    else{
-        gameongoing.value = true
-    }
-    status_msg.value = localStorage.getItem('status_msg')
+// if(localStorage.getItem('gameongoing')){
+//     if (localStorage.getItem('gameongoing')=='false'){
+//         gameongoing.value = false
+//     }
+//     else{
+//         gameongoing.value = true
+//     }
+//     status_msg.value = localStorage.getItem('status_msg')
 
-}
+// }
 
     (getword().then((data)=>{
 
@@ -85,12 +89,14 @@ if (data[0]['theme'].length<5){
     getword(true).then((data)=>{
                 clues.value = data[0]['theme'].slice(0,5)
                 word.value = data[1]
+                show_skeleton.value = false
             })
 
 }
 else{
     clues.value = data[0]['theme'].slice(0,5)
     word.value = data[1]
+    show_skeleton.value = false
 }
 
     }))
@@ -112,14 +118,15 @@ status_msg.value = msgs[1]
 gameongoing.value = false
 hidden_clues.value = clues.value.splice(1,4)
 
-localStorage.setItem("gameongoing", false)
-localStorage.setItem("status_msg", msgs[1])
+
+// localStorage.setItem("gameongoing", false)
+// localStorage.setItem("status_msg", msgs[1])
 
         return
     }
     else{    
         attempted_words.value.push(guess_word)}
-        localStorage.setItem("gameongoing", true)
+        // localStorage.setItem("gameongoing", true)
 
 
     if(guess_word.toLowerCase()!=word.value.toLowerCase()){
@@ -128,15 +135,16 @@ localStorage.setItem("status_msg", msgs[1])
         hidden_clues.value[guess_count.value-1] = clues.value[guess_count.value]
         current_guess.value = ''
 
-        localStorage.setItem("gameongoing", true)
+        // localStorage.setItem("gameongoing", true)
 
     }
     else {
         gameongoing.value = false
         hidden_clues.value = clues.value.splice(1,4)
         status_msg.value = msgs[0]
-        localStorage.setItem("gameongoing", false)
-        localStorage.setItem("status_msg", msgs[0])
+    
+        // localStorage.setItem("gameongoing", false)
+        // localStorage.setItem("status_msg", msgs[0])
 
     }
    
--- src/util/getword.js
+++ src/util/getword.js
@@ -1,5 +1,5 @@
 import { generate, count } from "random-words";
-let date = new Date()
+
 
 export async function getword(force = false){
 	let word = null
@@ -7,7 +7,8 @@ export async function getword(force = false){
 	word = "swim"
 	}
 	else{
-	word = generate({ min: 1, max: 1, seed: date.toDateString() })[0]
+	word = generate({ min: 1, max: 1, })[0]
+	
 	}
 	
     const url = `https://twinword-twinword-bundle-v1.p.rapidapi.com/word_theme/?entry=${word}`;
